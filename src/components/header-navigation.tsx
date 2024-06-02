9cc365c45391112cf93bb3a1b0bd5bbc2b465195
Wagner Taiatella <wtaiatella@gmail.com>
2024-06-02 17:36:20 -0300

    chore: header config for desktop and mobile

---
+++ src/components/header-navigation.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Menu, Smile, UserRound } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  navigationMenuTriggerStyle,
+} from "./hhh/navigation-menu";
+import NavigationMenuDemo from "./ui/examples/navigation-menu-shadcn";
+
+export default function HeaderNavigation() {
+  return (
+    <>
+      {/*mobile menu*/}
+      <NavigationMenu className="absolute md:relative md:hidden ml-2 ">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <Menu className="text-headertext size-10" />
+            </NavigationMenuTrigger>
+
+            <NavigationMenuContent className="list-none w-full p-2">
+              <NavigationMenuItems />
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      {/*desktop menu*/}
+      <NavigationMenu className="hidden md:block max-w-none ">
+        <NavigationMenuList className="text-headertext">
+          <NavigationMenuItems />
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
+  );
+}
+
+function NavigationMenuItems() {
+  return (
+    <>
+      <div className="w-full md:flex md:justify-around grow ">
+        <NavigationMenuItem>
+          <Link href="/events" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Events
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/suppliers" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Suppliers
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/recipes" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Recipes
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </div>
+
+      <div className="h-1 bg-zinc-200 dark:bg-zinc-800" />
+
+      <NavigationMenuItem className="flex items-center md:pr-2">
+        <UserRound className="md:text-headertext size-5 inline-block" />
+        <Link href="/login" legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Enter or Register
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+    </>
+  );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import HeaderNavigation from "./header-navigation";
 
 export default function Header() {
   return (
-    <header className="fixed t-0 l-0 mt-4 mx-0 w-full z-10 text-white">
+    <header className="fixed t-0 l-0 mt-4 mx-0 w-full z-10">
       <div className="container relative">
-        <div className="mx-4 relative flex align-center gap-4 items-center justify-between bg-black bg-opacity-50 rounded-[30px]">
-          <div className="flex grow gap-4 items-center justify-between">
-            <div className="mr-8 flex flex-col">
-              <p className="grow">Welcome :)</p>
-              <Link className="hover:underline" href="/login">
-                Enter or Register
-              </Link>
-            </div>
-
-            <div className="flex grow justify-around gap-4 text-xl mx-4">
-              <div>
-                <Link href="/events">Events</Link>
-              </div>
-              <div>
-                <Link href="/suppliers">Suppliers</Link>
-              </div>
-              <div>
-                <Link href="/recipes">Recipes</Link>
-              </div>
-            </div>
-          </div>
-          <Link href="/" className="ml-8 my-3">
+        <div className="mx-4 relative flex align-center items-center bg-black bg-opacity-50 dark:bg-opacity-90 rounded-[30px]">
+          <Link href="/" className="md:ml-8 my-3 mx-auto md:w-content">
             <Image
               src="/logo-yellow.png"
               className="h-24 w-36 min-w-36 cursor-pointer"
@@ -35,6 +16,7 @@ export default function Header() {
               alt="hhh logo"
             />
           </Link>
+          <HeaderNavigation />
         </div>
       </div>
     </header>
