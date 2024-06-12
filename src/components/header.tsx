969ede189779c3ecb509afb5b1710ec786457494
Wagner Taiatella <wtaiatella@gmail.com>
2024-06-12 07:21:01 -0300

    style: update header responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
   return (
     <header className="fixed t-0 l-0 mt-4 mx-0 w-full z-10">
       <div className="container relative">
-        <div className="mx-4 relative flex align-center items-center bg-black bg-opacity-50 dark:bg-opacity-90 rounded-[30px]">
+        <div className="relative flex align-center items-center bg-black bg-opacity-50 dark:bg-opacity-90 rounded-[30px]">
           <Link href="/" className="md:ml-8 my-3 mx-auto md:w-content">
             <Image
               src="/logo-yellow.png"
-              className="h-24 w-36 min-w-36 cursor-pointer"
+              className="2sm:h-24 2sm:w-36 h-16 w-24 cursor-pointer"
               width={768}
               height={512}
               alt="hhh logo"
