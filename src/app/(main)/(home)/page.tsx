95e341bbf8b64acb535f38a6f5015b080c91fdb8
Wagner Taiatella <wtaiatella@gmail.com>
2024-06-19 17:06:24 -0300

    chore: add stores and recipes section

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -6,6 +6,8 @@ import HeroSection from "./heroSection";
 import AnimatedTextSection from "./animatedTextSection";
 import IllustrativeSection from "./illustrativeSection";
 import EventSection from "./eventSection";
+import StoresSection from "./storesSection";
+import RecipeSection from "./recipeSection";
 
 export default function Home() {
   return (
@@ -14,9 +16,8 @@ export default function Home() {
       <AnimatedTextSection />
       <IllustrativeSection />
       <EventSection />
-      <Events />
-      <Suppliers />
-      <Recipes />
+      <StoresSection />
+      <RecipeSection />
       <Newsletter />
     </main>
   );
