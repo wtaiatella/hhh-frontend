fd9e5258e813501ec1fce05b5067f948e576874c
Wagner Taiatella <wtaiatella@gmail.com>
2024-07-02 16:42:25 -0300

    chore: comment manifest in Head metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
-  manifest: `${siteConfig.url}/site.webmanifest`,
+  // manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export const viewport: Viewport = {
