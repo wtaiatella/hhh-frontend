4d46bb4c60d804af9245057cb40e2649b19b14ce
Wagner Taiatella <wtaiatella@gmail.com>
2024-11-12 20:42:59 -0300

    feat: add prettier lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,93 +1,102 @@
-import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     extend: {
       colors: {
-        headertext: "hsl(var(--headertext))",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        highlight: 'var(--highlight)',
+        header: {
+          text: 'var(--header-text)',
+          menu: 'var(--header-menu)',
+          background: 'var(--header-background)',
+          hover: 'var(--header-hover)',
+        },
+        gradient: {
+          from: 'var(--gradient-from)',
+          to: 'var(--gradient-from)',
+        },
         primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
         },
         accent: {
-          DEFAULT: "var(--accent)",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'var(--card)',
+          foreground: 'var(--card-foreground)',
         },
-        highlight: "var(--highlight)",
       },
       fontFamily: {
-        sansInter: ["var(--font-sansInter)", ...fontFamily.sans],
-        serifBitter: ["var(--font-serifBitter)", ...fontFamily.serif],
-        monoJetBrains: ["var(--font-monoJetBrains)", ...fontFamily.mono],
+        sansInter: ['var(--font-sansInter)', ...fontFamily.sans],
+        serifBitter: ['var(--font-serifBitter)', ...fontFamily.serif],
+        monoJetBrains: ['var(--font-monoJetBrains)', ...fontFamily.mono],
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       screens: {
-        "2sm": "450px",
-        laptop: "1400px",
+        '2sm': '450px',
+        laptop: '1400px',
       },
       spacing: {
-        "76": "304px",
-        "114": "456px",
-        "120": "480px",
+        '76': '304px',
+        '114': '456px',
+        '120': '480px',
       },
       gridTemplateColumns: {
-        'auth': '1fr 2fr',
-      }
+        auth: '1fr 2fr',
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
