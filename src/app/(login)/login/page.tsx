46c6ea50c7b47907f469d4fb52dcf9ab0e703d11
Wagner Taiatella <wtaiatella@gmail.com>
2024-11-12 20:45:45 -0300

    chore: update login page

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,33 +1,32 @@
-import {Input} from "@/components/hhh/input";
-import React from "react";
+import { Input } from '@/components/hhh/input';
+import React from 'react';
 
 export default function Login() {
-    return (
-        <>
-            <div className="bg-red-500 p-4">
-                <p className="text-white">Login or Register</p>
-            </div>
-            <div className="bg-white p-24">
+  return (
+    <>
+      <div className="bg-gradient-to-r from to-bole-800 p-4">
+        <p className="text-white">Login or Register</p>
+      </div>
 
-                    <form>
-                        <div>
-                            <label htmlFor="username">Username:</label>
-                            <Input type="text" id="username" name="username" required/>
-                        </div>
-                        <div>
-                            <label htmlFor="password">Password:</label>
-                            <Input type="password" id="password" name="password" required/>
-                        </div>
-                        <button type="submit">Login</button>
-                    </form>
-                    <div>
-                        <a href="/signup">Don't have a login? Sign up here</a>
-                    </div>
-                    <div>
-                        <a href="/forgot-password">Forgot your password?</a>
-                    </div>
-                </div>
-        </>
-
-    );
+      <div className="bg-white p-24">
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <Input type="text" id="username" name="username" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div>
+          <a href="/signup">Don&apos;t have a login? Sign up here</a>
+        </div>
+        <div>
+          <a href="/forgot-password">Forgot your password?</a>
+        </div>
+      </div>
+    </>
+  );
 }
